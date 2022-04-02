8f3d4093800cec9811410848b06645a28a0b457d
Kuba <kuba3128@wp.pl>
2022-04-02 14:52:00 +0200

    pink_font

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,7 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    setSize (600, 400);
+    setSize (1200, 1200);
 }
 
 MainComponent::~MainComponent()
@@ -16,8 +16,8 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (33.0f));
-    g.setColour (juce::Colours::white);
+    g.setFont (juce::Font (40.0f));
+    g.setColour (juce::Colours::hotpink);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
